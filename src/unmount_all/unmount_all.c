71c585bc8ce78dd3a48512573bd9b154eb2e4b20
Tommimon <37435103+Tommimon@users.noreply.github.com>
2023-12-15 13:45:15 +0100

    Added comments

--- src/unmount_all/unmount_all.c
+++ src/unmount_all/unmount_all.c
@@ -7,23 +7,24 @@
 #include "queue.h"
 #include "rc.h"
 
+/* Contains all parameters passed to unmount_one() function */
 typedef struct t_args_t{
-    int index;
-    RC_STRING *path;
-    bool done;
-    pthread_cond_t signal;
-    pthread_mutex_t lock;
-    struct t_args_t *args_array;
+    int index;                    // index of the path in the list
+    RC_STRING *path;              // path to unmount
+    pthread_mutex_t unmounting;   // mutex locked until the path is unmounted
+    struct t_args_t *args_array;  // array of all parameters of all threads
 } thread_args_t;
 
+/* Pass arguments to mountinfo and store output in a list of paths to unmount */
 int populate_list(RC_STRINGLIST **list, int argc, char **argv)
 {
     FILE *fp;
     char path[4096]; // https://unix.stackexchange.com/questions/32795/what-is-the-maximum-allowed-filename-and-folder-size-with-ecryptfs
     int size;
-    char cmd[4096] = "/lib/rc/bin/mountinfo";
+    char cmd[4096] = "/lib/rc/bin/mountinfo";  //TODO: change to mountinfo
+                                               //TODO: find proper length for cmd
 
-    /* Craft the command with all passed arguments but the first*/
+    /* Craft the command with all passed arguments but the first */
     for (int i = 2; i < argc; i++) {
         printf("Argument: %s\n", argv[i]);
         strcat(cmd, " \"");
@@ -49,7 +50,6 @@ int populate_list(RC_STRINGLIST **list, int argc, char **argv)
         size++;
     }
 
-    /* close */
     pclose(fp);
 
     return size;
@@ -61,41 +61,30 @@ void *unmount_one(void *input)
 {
     thread_args_t *args = (thread_args_t *)input;
     RC_STRING *prev;
-    int i;
+    char command[4096];
+    int i, j;
 
-    //printf("Unmounting %s\n", args->path->value);
     prev = args->path;
     for(i = args->index-1; i >= 0; i--) {
         prev = TAILQ_PREV(prev, rc_stringlist, entries);
-        //printf("Previous: %s at index %d ", prev->value, i);
-        // get index of first different char 
-        int j = 0;
+        /* Get index of first different char */ 
+        j = 0;
         while (args->path->value[j] == prev->value[j]) {
             j++;
         }
-        //printf("First different char at: %d ", j);
-        //printf("First different char: %d\n", (int) args->path->value[j]);
-        // if the first different char is a '/' then we have a child
+        /* If the first different char is a '/' then we have a child */
         if (prev->value[j] == '/') {
-            //printf("Child: %s\n", prev->value);
-            /* Wait for child to unmount */
-            pthread_mutex_lock(&args->args_array[i].lock);
-            while (args->args_array[i].done == false) {
-                pthread_cond_wait(&args->args_array[i].signal, &args->args_array[i].lock);
-            }
-            pthread_mutex_unlock(&args->args_array[i].lock);
+            /* Wait for child to unmount (wait for mutex to be available) */
+            pthread_mutex_lock(&args->args_array[i].unmounting);
+            pthread_mutex_unlock(&args->args_array[i].unmounting);
         }
     }
 
-    char command[4096];
+    /* Unmount the path */
     sprintf(command, "umount %s", args->path->value);
     //system(command);
-    pthread_mutex_lock(&args->lock);
-    args->done = true;
     printf("Done %s\n", args->path->value);
     fflush(stdout);
-    pthread_cond_signal(&args->signal);
-    pthread_mutex_unlock(&args->lock);
 }
 
 int main(int argc, char **argv)
@@ -106,26 +95,30 @@ int main(int argc, char **argv)
 
     printf("Starting Unmount!\n");
 
+    /* Get list of paths to unmount */
     size = populate_list(&list, argc, argv);
 
-    pthread_t threads_list[size];
-    thread_args_t args[size];
+    pthread_t threads[size];
+    thread_args_t args_array[size];
 
+    /* Unmount each path in a different thread */
     i = 0;
     TAILQ_FOREACH(path, list, entries)
     {
-        args[i].index = i;
-        args[i].path = path;
-        args[i].done = false;
-        pthread_cond_init(&args[i].signal, NULL);
-        pthread_mutex_init(&args[i].lock, NULL);
-        args[i].args_array = args;
-        pthread_create(threads_list + i, NULL, unmount_one, args + i);
+        args_array[i].index = i;
+        args_array[i].path = path;
+        pthread_mutex_init(&args_array[i].unmounting, NULL);
+        pthread_mutex_lock(&args_array[i].unmounting);
+        args_array[i].args_array = args_array;
+        pthread_create(threads + i, NULL, unmount_one, args_array + i);
         i++;
     }
 
-    for (i = 0; i < size; i++)
-        pthread_join(threads_list[i], NULL);
+    /* Wait for all threads to finish */
+    for (i = 0; i < size; i++) {
+        pthread_join(threads[i], NULL);
+        pthread_mutex_unlock(&args_array[i].unmounting);
+    }
 
     rc_stringlist_free(list);
 
