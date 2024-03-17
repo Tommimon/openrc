ebf21a64d33e2bf69adc5e6bc468a143187ad2ed
DavidePellegrino64 <davideedoardo.pellegrino@polimi.it>
2024-03-17 15:31:33 +0100

    fixed a bug in exec_unmmount

--- src/do_unmount/do_unmount.c
+++ src/do_unmount/do_unmount.c
@@ -171,10 +171,9 @@ int exec_unmount(char *command, char *mount_point){
     int retry = 4;                  // Effectively TERM, sleep 1, TERM, sleep 1, KILL, sleep 1
     size_t len = 0;                 // length of the line read
     char *pids = NULL;              // line read from the fuser command
-    FILE *fp;                       // file pointer to the output of the command
+    FILE *fp = NULL;                // file pointer to the output of the command
     char *fuser_command;            // command to execute fuser and find the pids of the processes that use the mount point
     char *kill_command;             // command to kill the processes that use the mount point
-    char signal[4];                 // signal to send to the processes that use the mount point
     int killcheck;                  // return value of the kill command
     char pidString[10];             // string to store the pid
     enum Outcome retVal = SUCCESS;  // return value of the function
